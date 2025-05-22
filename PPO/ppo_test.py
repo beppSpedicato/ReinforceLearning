0841e70c7ff81d3102129b1ab01f2de4ff041721
giuseppe spedicato <giuseppespedicato.380@gmail.com>
2025-05-22 16:39:05 +0200

    PPO test

--- PPO/ppo_test.py
+++ PPO/ppo_test.py
@@ -8,45 +8,28 @@ from env.custom_hopper import *
 from stable_baselines3 import PPO
 import matplotlib.pyplot as plt
 
-def plotRewards (train_rewards):
-	plt.figure(figsize=(10, 5))
-	plt.plot(train_rewards, label=f'TEST reward per episode for model reinforce b20')
-	plt.xlabel('Episode')
-	plt.ylabel('Reward')
-	plt.title('Test Rewards')
-	plt.legend()
-	plt.grid(True)
-	plt.tight_layout()
-	plt.savefig(f"test_rewards_for_model_PPO.png")
-	plt.close()
 
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--model', default=None, type=str, help='Model path')
-    parser.add_argument('--device', default='cpu', type=str, help='network device [cpu, cuda]')
-    parser.add_argument('--render', default=False, action='store_true', help='Render the simulator')
-    parser.add_argument('--episodes', default=1000, type=int, help='Number of test episodes')
-
-    return parser.parse_args()
-
-args = parse_args()
-
-def main():
-
-	# env = gym.make('CustomHopper-source-v0')
-	env = gym.make('CustomHopper-target-v0')
+def test_ppo_policy(
+    test_env: str = 'CustomHopper-target-v0',
+    model: str = "ppo_model.mdl",
+    episodes: int = 1000,
+    render: bool = False
+):
+	env = gym.make(test_env)
 
 	print('Action space:', env.action_space)
 	print('State space:', env.observation_space)
 	print('Dynamics parameters:', env.get_parameters())
 	
-	agent = PPO.load(args.model)
+	agent = PPO.load(model)
 	state = env.reset()
 	test_rewards = []
+	episode_lengths = [] 
 
-	for episode in range(args.episodes):
+	for episode in range(episodes):
 		done = False
 		test_reward = 0
+		episode_length = 0
 		state = env.reset()
 
 		while not done:
@@ -55,16 +38,13 @@ def main():
 
 			state, reward, done, info = env.step(action)
 
-			if args.render:
+			if render:
 				env.render()
 
 			test_reward += reward
+			episode_length += 1
 		
 		test_rewards.append(test_reward)
-		print(f"Episode: {episode} | Return: {test_reward}")
+		episode_lengths.append(episode_length)
 
-	plotRewards(test_rewards)
-
-
-if __name__ == '__main__':
-	main()
+	return test_rewards, episode_lengths
