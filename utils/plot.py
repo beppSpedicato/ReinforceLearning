2d5c9a6552a895c914c37d9b3201fd3bbfd607a4
lorycala <126784107+lorycala@users.noreply.github.com>
2025-05-27 19:36:21 +0200

    Update plot.py

--- utils/plot.py
+++ utils/plot.py
@@ -42,32 +42,7 @@ def plotTrainRewards(
 			for mean_value in means:
 				f.write(f"{mean_value}\n")
 
-def plotAvgTxtFiles(
-    file_list, 
-    merge_title,
-	x_label: str = 'Window index (every 500 episodes)',
-	y_label: str = 'Mean reward',
-	title: str = 'Mean rewards for each baselines'
-):
-	plt.figure(figsize=(12, 6))
 
-	for filename in file_list:
-		with open(filename, 'r') as f:
-			lines = f.readlines()
-			baseline = lines[0].strip()
-			means = [float(line.strip()) for line in lines[1:]]
-
-		x = [i for i in range(len(means))]
-		plt.plot(x, means, marker='o', label=baseline)
-
-	plt.xlabel(x_label)
-	plt.ylabel(y_label)
-	plt.title(title)
-	plt.legend()
-	plt.grid(True)
-	plt.tight_layout()
-	plt.savefig(f"{merge_title}.png")
-	plt.close()
 
 
 
